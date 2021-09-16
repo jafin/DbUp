c1bc05c9506a37428201f5e1abe8a166cfc5894b
Jason Finch <jason.finch@gmail.com>
2021-09-17 08:38:25 +1000

    fix (safehandlererror): Fix ExhibitSafeHandler error fixed in later version of SQLite dependencies.
    * Warn: changes minimum targetFramework to netstandard2.0 (downstream dependency minimum)
    
    Resovles #577

--- src/dbup-tests/Support/SQLite/SQLiteSupportTests.cs
+++ src/dbup-tests/Support/SQLite/SQLiteSupportTests.cs
@@ -1,8 +1,7 @@
-﻿#if !NETCORE
-using System;
+﻿using System;
 using System.Data.SQLite;
 using System.IO;
-using NUnit.Framework;
+using Shouldly;
 using Xunit;
 
 namespace DbUp.Tests.Support.SQLite
@@ -26,6 +25,26 @@ namespace DbUp.Tests.Support.SQLite
                 .WithScript("Script0001", "CREATE TABLE IF NOT EXISTS Foo (Id int)")
                 .Build();
         }
+
+        [Fact]
+        public void DoesNotExhibitSafeHandleError_Issue577() 
+        {
+            var connectionString = "Data source=:memory:";
+
+            var upgrader =
+                DeployChanges.To
+                    .SQLiteDatabase(connectionString)
+                    .WithScript("Script001", @"
+create table test (
+    contact_id INTEGER PRIMARY KEY
+);
+")
+                    .LogScriptOutput()
+                    .LogToConsole()
+                    .Build();
+            var result = upgrader.PerformUpgrade();
+            result.Successful.ShouldBeTrue();
+
+        }
     }
 }
-#endif
