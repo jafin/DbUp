beeaf939d911ea3ff614d514b229db4a745bbd2f
Jason Finch <jason.finch@gmail.com>
2021-09-17 08:36:56 +1000

    Add SQL Parser test that fails due to delimiter, should be using a replacement delimiter.

---
+++ src/dbup-tests/Support/MySql/Parser/SqlParserTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DbUp.MySql;
+using Shouldly;
+using Xunit;
+
+namespace DbUp.Tests.Support.MySql.Parser
+{
+    public class MySqlParserTests
+    {
+        private int commandCount = 0;
+
+        [Fact]
+        public void Parse()
+        {
+            MySqlCommandReader reader = new MySqlCommandReader(@"
+DROP PROCEDURE IF EXISTS My_StoreProc;
+
+CREATE PROCEDURE My_StoreProc(strEmail VARCHAR(255))
+BEGIN
+SELECT id FROM table_name WHERE email = strEmail
+ORDER BY id DESC limit 1;
+END;
+");
+            reader.ReadAllCommands(CommandHandler);
+            commandCount.ShouldBe(2);
+        }
+
+        private void CommandHandler(string val)
+        {
+            Console.WriteLine(val);
+            commandCount++;
+        }
+    }
+}
